02d92d00571179ef48fd07a1b25b2c765f1e383e
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-03-20 20:24:15 +0200

    first exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import configparser
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType
+from pyspark.sql.functions import sqrt, pow
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -9,12 +10,22 @@ e, k = int(config["query_config"]["e"]), int(config["query_config"]["k"])
 
 spark = SparkSession.builder.getOrCreate()
 
-schema = StructType([
-    StructField("id", IntegerType(), True),
-    StructField("x", IntegerType(), True),
-    StructField("y", IntegerType(), True)
+r_schema = StructType([
+    StructField("r_id", IntegerType(), True),
+    StructField("r_x", IntegerType(), True),
+    StructField("r_y", IntegerType(), True)
 ])
 
+s_schema = StructType([
+    StructField("s_id", IntegerType(), True),
+    StructField("s_x", IntegerType(), True),
+    StructField("s_y", IntegerType(), True)
+])
 
-r_df = spark.read.csv("data/RAILS.csv", sep="\t", header=False, schema=schema) 
-s_df = spark.read.csv("data/AREALM.csv", sep="\t", header=False, schema=schema) 
+r_df = spark.read.csv("data/RAILS.csv", sep="\t", header=False, schema=r_schema) 
+s_df = spark.read.csv("data/AREALM.csv", sep="\t", header=False, schema=s_schema) 
+
+df_cross = r_df.join(s_df, (sqrt(pow(r_df.r_x - s_df.s_x, 2) + pow(r_df.r_y - s_df.s_y, 2)) < e))
+
+with open("output/resultsA.txt", "w") as f1:
+    f1.write(str(df_cross.count()))
