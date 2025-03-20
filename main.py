1c316a0d11812a9f16d9ffc5bd411258846d6557
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-03-20 18:12:28 +0200

    read parameters from ini file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-pass
+import configparser
+from pyspark.sql import SparkSession
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+e, k = int(config["query_config"]["e"]), int(config["query_config"]["k"])
+
+spark = SparkSession.builder.getOrCreate()
+
